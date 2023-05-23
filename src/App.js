91ddc5279fa09e2328330f1fa62c648046bc093b
Eder <ederfornero.ef@gmail.com>
2023-05-23 14:40:53 -0300

    todo list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+
 import './App.css';
+import Task from './svg/task-svgrepo-com.svg'
+
 
 function App() {
+
+  const [item, setItem] = useState('');
+  const [items, setItems] = useState([]);
+
+  const addItem = () => {
+
+    if (!item) {
+      alert('There must be an item')
+      return;
+    }
+
+    const newItem = {
+      id: Math.floor(Math.random() * 1000),
+      value: item
+    };
+    setItems(oldList => [...oldList, newItem]);
+    setItem('');
+    console.log(items);
+  }
+
+  const deleteItem = (id) => {
+    const newArray = items.filter(item => item.id !== id);
+    setItems(newArray);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/*Header*/}
+      <div className='div-todo-header'>
+        <h1 className='h1-todo'>To Do List App</h1>
+        <img className='todo-svg' src={Task} alt="Todo svg" />
+      </div>
+
+      {/*Add a task*/}
+      <div className='div-input'>
+        <input
+          type="text"
+          placeholder='Add an item here...'
+          value={item}
+          onChange={e => setItem(e.target.value)}
+        />
+        <button onClick={() => addItem()}>Add</button>
+      </div>
+
+      {/*List*/}
+      <div className='div-ul'>
+        <ul>
+
+          {items.map(i => {
+            return (
+              <>
+                <li key={i.id}>
+                  {i.value}
+                  <button className='delete-button' onClick={() => deleteItem(i.id)}>❌</button>
+                </li>
+              </>
+            )
+          })}
+
+        </ul>
+      </div>
+    </>
   );
 }
 
